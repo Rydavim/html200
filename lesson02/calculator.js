e9dcb16442c8743a06e34adde3321bc211019675
Rydavim <rydavim@gmail.com>
2022-01-17 11:25:28 -0500

    added calculator.js file

---
+++ lesson02/calculator.js
@@ -0,0 +1,13 @@
+// all vars static - const
+const result = "Calculation Result: ";
+const num1 = 4;
+const num2 = 7;
+const num3 = 120;
+const flt1 = 1.25;
+const flt2 = 42.42;
+
+console.log(result + (num1 * num2)); // multiply
+console.log(result + (num3 / num1)); // divide
+console.log(result + (num2 + flt2)); // add
+console.log(result + (num1 - flt1)); // subtract
+console.log(result + (num3 % num2)); // remainder
