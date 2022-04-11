a6ea85e57858b2d0541425caf9e37fad2e869eb2
Rydavim <rydavim@gmail.com>
2022-04-11 11:54:28 -0400

    reworked banking functions for cleaner loop

--- lesson05/banking.js
+++ lesson05/banking.js
@@ -1,15 +1,21 @@
-banking();
-function banking(balance = 0) {
+let balance = 0;
+let transacting = true;
+while (transacting) {
   let userSelection = window.prompt("Enter Q to Quit\nEnter W to Withdraw\nEnter D to Deposit\nEnter B to View Balance\n");
   switch(userSelection) {
-    case 'Q': { break; }
+    case 'Q': {
+      transacting = false;
+      break;
+    }
     case 'W': {
       let withdrawl = window.prompt("How much would you like to withdraw? ");
-      return balance = balance - withdrawl;
+      balance -= parseInt(withdrawl);
+      break;
     }
     case 'D': {
       let deposit = window.prompt("How much would you like to deposit? ");
-      return balance = balance + deposit;
+      balance += parseInt(deposit);
+      break;
     }
     case 'B': {
       alert(balance);
